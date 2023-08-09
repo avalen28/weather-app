9269ccddfef2af159f00f7047ae9771c217a2842
Alberto Valenzuela <avalen28@gmail.com>
2023-08-09 14:53:00 +0200

    Adds gelocation obtained from the b rowser into the AreaSelector component and sets the value of the latitude and longitude states when the component loads for the first time.

--- src/views/AreaSelector.jsx
+++ src/views/AreaSelector.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 // Import components
 import Location from '../components/Location';
 import Area from '../components/Area';
 
 const AreaSelector = () => {
-    return (
-        <div>
-            <Location />
-            <Area />
-        </div>
-    );
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            });
+        }
+    }, [])
+
+  return (
+    <div>
+      <Location />
+      <Area />
+    </div>
+  );
 };
 
 export default AreaSelector;
