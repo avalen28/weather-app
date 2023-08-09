1ca3eda9bdc5c54924156cb0854b6f53a8893523
Alberto Valenzuela <avalen28@gmail.com>
2023-08-09 17:28:47 +0200

    Adds the interaction for the user to control the zoom level, and the component that updates the map on each change

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Circle } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Circle, useMap } from "react-leaflet";
 import { Icon } from "leaflet";
 
+const UpdateMap = ({center, zoom, zoomControl}) => {
+  // Except for its children, MapContainer props are immutable: changing them after they have been set a first
+  // time will have no effect on the Map instance or its container.The Leaflet Map instance created by the MapContainer 
+  // element can be accessed by child components using one of the provided hooks or the MapConsumer component.
+  const map = useMap();
+  map.setView(center, zoom, zoomControl);
+  return null;
+};
+
 const Area = ({ coordinates: { latitude, longitude } }) => {
+  const [zoom, setZoom] = useState(13);
   const icon = new Icon({
     iconUrl:
       "https://www.citypng.com/public/uploads/preview/red-gps-location-symbol-icon-hd-png-116369431412wisxxt5bl.png",
     iconSize: [38, 38],
   });
   const position = [latitude, longitude];
-
+  const handleZoom = (e) => setZoom(Number(e.target.value));
   return (
-    <MapContainer center={position} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <>
+      <input
+        type="range"
+        name="zoom"
+        min="1"
+        max="18"
+        value={zoom}
+        onChange={handleZoom}
       />
-      <Marker position={position} icon={icon}></Marker>
-      <Circle center={position} radius={1000} />
-    </MapContainer>
+      <MapContainer center={position} zoom={zoom} zoomControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={icon}></Marker>
+        <Circle center={position} radius={1000} />
+        <UpdateMap center={position} zoom={zoom} zoomControl={false} />
+      </MapContainer>
+    </>
   );
 };
 
