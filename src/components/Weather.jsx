33363009f1901a3919e24decaa99d4d6a0031f9f
Alberto Valenzuela <avalen28@gmail.com>
2023-08-09 19:50:17 +0200

    Adds API call with Axios. Manage data and print it on basic HTML. IMPORTANT! some cities have the wrong name on the json file

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Weather = () => {
-    return <div>this is the Weather component</div>;
+// 249efd60e5021ba25f979f2caac2b853
+
+const Weather = ({ currentCity }) => {
+  const API_KEY = "249efd60e5021ba25f979f2caac2b853";
+  const basicWeather = {
+    weather: "-",
+    description: "-",
+    sunset: "-",
+    sunrise: "-",
+    location: "-",
+    temperature: "-",
+    feelsLike: "-",
+    humidity: "-",
+  };
+  const [currentWeather, setCurrentWeather] = useState(basicWeather);
+  const getWeatherFromAPI = async () => {
+    try {
+      const weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_KEY}&units=metric`
+      );
+        console.log(weatherResponse)
+       
+          const newWeatherInfo = weatherResponse.data;
+          setCurrentWeather((prev) => ({
+            ...prev,
+            weather: newWeatherInfo.weather[0].main,
+            description: newWeatherInfo.weather[0].description,
+            sunset: newWeatherInfo.sys.sunset,
+            sunrise: newWeatherInfo.sys.sunrise,
+            location: newWeatherInfo.name,
+            temperature: newWeatherInfo.main.temp,
+            feelsLike: newWeatherInfo.main.feels_like,
+            humidity: newWeatherInfo.main.humidity,
+          }));
+        
+      } catch (error) {
+      setCurrentWeather(
+        basicWeather
+        );
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getWeatherFromAPI();
+    // eslint-disable-next-line
+  }, [currentCity]);
+  return (
+    <div>
+      <div className="block-1">
+        <img src="" alt="" />
+        <div>
+          <p>weather</p>
+          <p>{currentWeather.weather}</p>
+        </div>
+        <div>
+          <p>description</p>
+          <p>{currentWeather.description}</p>
+        </div>
+      </div>
+      <div className="block-2">
+        <div>
+          <p>sunset</p>
+          <p>{currentWeather.sunset}</p>
+        </div>
+        <div>
+          <p>sunrise</p>
+          <p>{currentWeather.sunrise}</p>
+        </div>
+        <div>
+          <p>location</p>
+          <p>{currentWeather.location}</p>
+        </div>
+      </div>
+      <div className="block-3">
+        <div>
+          <p>temperature</p>
+          <p>{currentWeather.temperature}</p>
+        </div>
+        <div>
+          <p>feels like</p>
+          <p>{currentWeather.feelsLike}</p>
+        </div>
+      </div>
+      <div className="block-4">
+        <p>humidity</p>
+        <p>{currentWeather.humidity}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
